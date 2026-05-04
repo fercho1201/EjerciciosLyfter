9330cd92341ee16893aeb22dc80a5b0033491f55
Cris Ruiz <fercho5523@gmail.com>
2026-05-04 18:02:41 -0400

    Cambie mi edad en uno de los ejercicios de diccionarios (create_2_lists.py) y tambien cree una discripcion para que sea mas facil de verificar

--- Python Basico/Ejercicios_de_Diccionarios/create_2_lists.py
+++ Python Basico/Ejercicios_de_Diccionarios/create_2_lists.py
@@ -1,6 +1,6 @@
 list1 = ['first name', 'last name', 'age']
-list2 = ['Cris', 'Ruiz', 35]
-
+list2 = ['Cris', 'Ruiz', 36]
+#just corrected my age and wanted to probe that the changes are beeing made correctly thru github.
 dictionary = {}
 
 for data in list1:
